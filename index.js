d3fab73f96546dfcbae7bb8b197559fba6250d25
Stone98 <stone18003@gmail.com>
2021-04-16 08:52:14 -0700

    index comments

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const server = require("./api/server.js");
+const server = require("./api/server.js"); // hooks up the server to index.js so it can run with either node or nodemon
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000; // uses PORT variable in .env or defaults to 4000
 
 server.listen(PORT, () => {
+  // tells server what port to run on
   console.log(`Listening on port ${PORT}...`);
 });
