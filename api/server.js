f311ce0f5d32614e9bccf5ad2f32895c6b128cca
Stone98 <stone18003@gmail.com>
2021-04-16 08:50:00 -0700

    server comments

--- api/server.js
+++ api/server.js
@@ -5,9 +5,17 @@ const ResourceRouter = require("./resource/router");
 const ProjectRouter = require("./project/router");
 const TaskRouter = require("./task/router");
 
+// global middlewares
 server.use(express.json());
+
+// enables routes for resources, projects and tasks
 server.use("/api/resources", ResourceRouter);
 server.use("/api/projects", ProjectRouter);
 server.use("/api/tasks", TaskRouter);
+
+// catch all endpoint that indicates that server/api is up
+server.use("*", (req, res) => {
+  res.send(`<h1>API is up!</h1>`);
+});
 
 module.exports = server;
