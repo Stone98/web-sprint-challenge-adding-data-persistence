46eeb48eef54ea8aee41e40cf85b01d690a493e2
Stone98 <stone18003@gmail.com>
2021-04-16 09:04:15 -0700

    resources comments

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,25 +1,30 @@
-const router = require("express").Router();
-const Resource = require("./model");
+const router = require("express").Router(); // sets up resources router
+const Resource = require("./model"); // imports resources model functions
 
+// route to GET all resources
 router.get("/", async (req, res, next) => {
   try {
-    const data = await Resource.getAll();
-    res.status(200).json(data);
+    const data = await Resource.getAll(); // store all resources in a data variable
+    res.status(200).json(data); // respond with all resources
   } catch (err) {
+    // if error occurs, send it to the error handling middleware
     next(err);
   }
 });
 
+// route to POST a new resource
 router.post("/", async (req, res, next) => {
   try {
     const resource = req.body;
-    const data = await Resource.create(resource);
-    res.status(201).json(data);
+    const data = await Resource.create(resource); // store new resource in data variable
+    res.status(201).json(data); // respond with newly created resource
   } catch (err) {
+    // if error occurs, send it to the error handling middleware
     next(err);
   }
 });
 
+// error handling middleware for errors that occur in resources router
 // eslint-disable-next-line no-unused-vars
 router.use((err, req, res, next) => {
   // eslint-disable-line
